c37829e5e21864130dd46b59bd34f9c386823495
gandharkulkarni <gakulkarni@dons.usfca.edu>
2024-03-06 13:32:41 -0800

    Minor bug fixes

--- compile.c
+++ compile.c
@@ -11,25 +11,28 @@ void compile_init(char* name) {
     printf(".global codegen_func_s\n");
     printf("codegen_func_s:\n");
     printf("%s:\n", name);
-    // printf("\taddi sp, sp, -32\n");
-    // printf("\tsd ra, (sp)\n");
+    printf("\taddi sp, sp, -8\n");
+    printf("\tsd ra, (sp)\n");
 }
 void compile_exit() {
-    printf("\tmv a0, t0\n");
-    // printf("\tld ra, (sp)\n");
-    // printf("\taddi sp, sp, 32\n");
+    printf("\tld a0, (sp)\n");
+    printf("\taddi sp, sp, 8\n");
+    printf("\tld ra, (sp)\n");
+    printf("\taddi sp, sp, 8\n");
     printf("\tret\n");
 }
 
 void compile_unary_operation(struct parse_node_st *pt) {
     if (pt->oper1.oper == OP_MINUS) {
+        printf("\tsub t0, zero, t0\n");    
+    } else if (pt->oper1.oper == OP_NOT) {
         printf("\tli t1, -1\n");
         printf("\txor t0, t0, t1\n");
-    } else if (pt->oper1.oper == OP_NOT) {
-        printf("\tsub t0, zero, t0\n");
     } else {
    		compile_error("Bad operator");
     }
+    printf("\taddi sp, sp, -8\n");
+    printf("\tsd t0, (sp)\n");
 }
 
 void compile_binary_operation(struct parse_node_st *pt) {
@@ -61,13 +64,17 @@ void compile_binary_operation(struct parse_node_st *pt) {
 }
 
 void compile (struct parse_node_st *pt) {
-    if (pt->type == EX_REG) {
+    if (pt->type == EX_INTVAL){
         printf("\taddi sp, sp, -8\n");
-        // printf("\tli t0, %d\n", config->args[eval_get_register_value(pt->reg.name)]);
+        printf("\tli t0, %d\n", pt->intval.value);
+        printf("\tsd t0, (sp)\n");
+    }
+    else if (pt->type == EX_REG) {
+        printf("\taddi sp, sp, -8\n");
         printf("\tsd %s, (sp)\n", pt->reg.name);
     } else if (pt->type == EX_OPER1) {
         compile(pt->oper1.operand);
-        printf("\tld t0, (sp)");
+        printf("\tld t0, (sp)\n");
         printf("\taddi sp, sp, 8\n");
         compile_unary_operation(pt);
     } else if (pt->type == EX_OPER2) {
